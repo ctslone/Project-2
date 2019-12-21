b0fa3d798ea70930f9c3f94cab667b067dd56fed
Parth Parmar <parthmparmar@gmail.com>
2019-12-21 12:00:16 -0500

    search functions checks against internal db, and renders page as needed. upBoat and downBoat are now working.

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,4 @@
 DROP DATABASE IF EXISTS risingTidedb;
 CREATE DATABASE risingTidedb;
 
-CREATE TABLE moviesList;
-(
-	id int NOT NULL AUTO_INCREMENT,
-	author varchar(255) NOT NULL,
-	quote TEXT NOT NULL,
-	PRIMARY KEY (id)
-);
+-- We are using Serialize to create the tables in mysql
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,54 +2,78 @@ var db = require("../models");
 var helperFunctions = require("../routes/helperFunctions.js");
 var findMovie = helperFunctions.findMovie;
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/examples", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
   // Add new movie to db
-  app.post("/api/movie/add/:imdbId", function(req, res){
-    imdbId = req.params.imdbId;
-    console.log("server" + imdbId)
-    findMovie(imdbId, function(results){
-    console.log(results);
+  app.post("/api/movie/add/:imdbID", function (req, res) {
+    imdbID = req.params.imdbID;
+    findMovie(imdbID, function (results) {
+      console.log(results);
 
-    newMovie = { 
-    imdbid: results.imdbID, 
-    title: results.Title,
-    year: results.Year,
-    rated: results.Rated,
-    runtime: results.Runtime,
-    genre: results.Genre,
-    director: results.Director,
-    actors: results.Actors,
-    plot: results.Plot,
-    language: results.Language,
-    poster: results.Poster,
-    boatsValue: 1
-    }
+      newMovie = {
+        imdbid: results.imdbID,
+        title: results.Title,
+        year: results.Year,
+        rated: results.Rated,
+        runtime: results.Runtime,
+        genre: results.Genre,
+        director: results.Director,
+        actors: results.Actors,
+        plot: results.Plot,
+        language: results.Language,
+        poster: results.Poster,
+        boatsValue: 1
+      }
 
-    db.moviesList.create(newMovie)
+      db.moviesList.create(newMovie)
       res.send("Movie Added to db");
     });
 
   });
 
+  // downBoat
+  app.put("/api/movie/up-boat/:imdbID", function (req, res) {
+    var imdbID = req.params.imdbID;
+    console.log(imdbID)
+    db.moviesList.increment({boatsValue:1},{where:{imdbid: imdbID}}).then(function(results){
+      console.log(results);
+      res.send("Movie was upBoated by 1");
+    }).catch(function(results){
+      console.log(results);
+      res.send("error during upBoat");
+    });
+  });
+
+      // downBoat
+  app.put("/api/movie/down-boat/:imdbID", function (req, res) {
+    var imdbID = req.params.imdbID;
+    console.log(imdbID)
+    db.moviesList.decrement({boatsValue:1},{where:{imdbid: imdbID}}).then(function(results){
+      console.log(results);
+      res.send("Movie was downBoated by 1");
+    }).catch(function(results){
+      console.log(results);
+      res.send("error during downBoat");
+    });
+  });
 };
